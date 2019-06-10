10af89d1d98acff340d1e14257f08cc0b782677e
hsoh1990 <ohs4123@gmail.com>
2019-06-11 08:39:50 +0900

    jpa 기본 설정 및 사용

---
+++ src/main/java/io/wellstone/lecturejpawhiteship/account/Account.java
@@ -0,0 +1,21 @@
+package io.wellstone.lecturejpawhiteship.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
