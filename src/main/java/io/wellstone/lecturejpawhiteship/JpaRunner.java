04eb78c403bfbe8ad19618f793a6f91ab0a673af
hsoh1990 <ohs4123@gmail.com>
2019-06-15 15:07:36 +0900

    spring jpa 기본 동작 원리

--- src/main/java/io/wellstone/lecturejpawhiteship/JpaRunner.java
+++ src/main/java/io/wellstone/lecturejpawhiteship/JpaRunner.java
@@ -1,17 +1,12 @@
 package io.wellstone.lecturejpawhiteship;
 
 import io.wellstone.lecturejpawhiteship.post.Post;
+import io.wellstone.lecturejpawhiteship.post.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -19,33 +14,11 @@ import java.util.List;
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager entityManager;
+    @Autowired
+    PostRepository postRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        jpql..
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
-
-        List<Post> jpqlPosts = query.getResultList();
-        System.out.println("jpql ....");
-        jpqlPosts.forEach(System.out::println);
-
-//        criteria..
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Post> criteria = builder.createQuery(Post.class);
-        Root<Post> root = criteria.from(Post.class);
-        criteria.select(root);
-        List<Post> criteriaPosts = entityManager.createQuery(criteria).getResultList();
-        System.out.println("criteria ....");
-        criteriaPosts.forEach(System.out::println);
-
-//        NativeQuery..
-        List<Post> nativeQueryPosts = entityManager.createNativeQuery("SELECT * FROM POST", Post.class).getResultList();
-
-        System.out.println("NativeQuery ....");
-        nativeQueryPosts.forEach(System.out::println);
-
-
+        postRepository.findAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/io/wellstone/lecturejpawhiteship/post/PostRepository.java
@@ -0,0 +1,7 @@
+package io.wellstone.lecturejpawhiteship.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
