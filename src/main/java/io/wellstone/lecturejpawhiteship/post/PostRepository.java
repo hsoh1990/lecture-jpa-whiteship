33367c891d027999600366bd271db6296e530276
hsoh1990 <ohs4123@gmail.com>
2019-06-17 17:33:08 +0900

    Common Repository

--- src/main/java/io/wellstone/lecturejpawhiteship/post/PostRepository.java
+++ src/main/java/io/wellstone/lecturejpawhiteship/post/PostRepository.java
@@ -1,7 +1,13 @@
 package io.wellstone.lecturejpawhiteship.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByTitleContaining(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }
