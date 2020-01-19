5ad0cde5d959f2520c01ac3d17702c85e518b123
hsoh1990 <ohs4123@gmail.com>
2020-01-19 21:23:02 +0900

    JPA Named Parameter AND SpEL

--- src/main/java/io/wellstone/lecturejpawhiteship/post/PostRepository.java
+++ src/main/java/io/wellstone/lecturejpawhiteship/post/PostRepository.java
@@ -4,6 +4,7 @@ package io.wellstone.lecturejpawhiteship.post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleStartingWith(String title);
 
-    @Query("select p from Post AS p where p.title = ?1")
-    List<Post> findByTitle(String title, Sort sort);
+//    @Query("select p from Post AS p where p.title = ?1")
+    @Query("select p from #{#entityName} AS p where p.title = :title")
+    List<Post> findByTitle(@Param("title") String title, Sort sort);
 }
